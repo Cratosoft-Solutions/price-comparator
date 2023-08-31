800b3c1af42fc3afb6af86519a6d7baa3b3b7151
Randall <rcotojimenez@gmail.com>
2023-08-31 09:51:03 -0600

    Cambios error window

--- main/components/ScrollToTop.jsx
+++ main/components/ScrollToTop.jsx
@@ -3,22 +3,25 @@ import { BsArrowUpCircle } from "react-icons/bs";
 import {useState, useEffect} from "react";
 
 const ScrollToTopButton  =() => {
-    const [scrollYPosition, setScrollYPosition]= useState(window?.scrollY);
-    const isBrowser = () => typeof window !== 'undefined'; 
+  const isBrowser = () => typeof window !== 'undefined'; 
+
+    const [scrollYPosition, setScrollYPosition]= useState(isBrowser()?window.scrollY:0);
 
     function scrollToTop() {
-        if (!isBrowser()) return;
+        if (isBrowser()){
            window.scrollTo({ top: 0, behavior: 'smooth' });
+          }
     }
 
     const onScroll = (e) => {
-        if (!isBrowser()) return;
+        if (isBrowser()){
         setScrollYPosition(window.scrollY);
+      }
       }
 
     useEffect(() => {
-        if (!isBrowser()) return;
-        window.addEventListener('scroll', onScroll);
+        if (isBrowser()) {
+        window.addEventListener('scroll', onScroll);}
     },[]);
 
     return (
