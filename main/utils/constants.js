9040e1e9037cd43cd72157d7d183b45fe1d95b8d
josermv <jotarmv@gmail.com>
2023-08-30 22:46:20 -0600

    JMONTOYA Cambios 30-08-2023

--- main/utils/constants.js
+++ main/utils/constants.js
@@ -5,18 +5,21 @@ export const SEARCH_DEFAULT_OPTIONS = {
 export const COINCIDENCES = "COINCIDENCES";
 
 export const CATEGORIES = [
-  { value: -1, label: "Categoria" },
-  { value: 1, label: "Supermercados" },
-  { value: 2, label: "Farmacias" },
-  { value: 3, label: "Abarrotes" },
-  { value: 4, label: "Perfumeria" },
-  { value: 5, label: "Otros" },
-];
+  {value:-1, label:"Categoria"},
+  {value:1, label:"Supermercados"},
+  {value:2, label:"Farmacias"},
+  {value:3, label:"Tecnología / Electrónica / Linea Blanca"},
+  {value:4, label:"Ferreteria"},
+  {value:5, label:"Perfumeria"},
+  {value:6, label:"Otros"}
+]
 
 export const STORE_BY_CATEGORY = [
-  { category: 1, stores: [1, 2, 3, 4, 5] },
-  { category: 2, stores: [6, 7, 8, 9, 10] },
-  { category: 3, stores: [11, 12, 13, 14, 15] },
-  { category: 4, stores: [16, 17, 18, 19, 20] },
-  { category: 5, stores: [21, 22, 23, 24, 25] },
-];
+{category:1, stores:[1,2,3,4,5]},
+{category:2, stores:[6,7,8,10]},
+{category:3, stores:[2,3,11,12,13,14,15,16,17,18,19,20,21]},
+{category:4, stores:[22,23,24,25,26]},
+{category:5, stores:[13,20,21,22,27,30]}, 
+{category:6, stores:[31,32,33,34,35]}
+]
+
