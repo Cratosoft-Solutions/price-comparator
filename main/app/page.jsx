ce23d05edeb55b2cac1d6bb7397319dab34f1935
Randall <rcotojimenez@gmail.com>
2024-01-24 23:32:22 -0600

    Ajustes para SEO

--- main/app/page.jsx
+++ main/app/page.jsx
@@ -1,19 +1,9 @@
 "use client";
 import Link from "next/link";
-import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Encuéntralo Fácil CR</title>
-        <link rel="canonical" href="https://www.encuentralofacilcr.com" />
-        <meta
-          name="Encuéntralo Fácil CR"
-          content="tu destino en línea para descubrir una amplia variedad de bienes y servicios en Costa Rica"
-        />
-      </Helmet>
       <section className="mx-auto mb-10 px-2.5 text-center sm:px-0 ">
         <h1 className="w-full mt-5 font-display text-4xl font-bold	 leading-[1.15] text-black sm:text-6xl sm:leading-[1.15]">
           Encuentralo Fácil CR 
