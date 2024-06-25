9f2246879aed1375a9fa0cca61708808fc81ac77
Randall <rcotojimenez@gmail.com>
2024-06-25 17:45:50 -0600

    Ajustes mínimos UI

--- main/components/NavCategoriesBar.jsx
+++ main/components/NavCategoriesBar.jsx
@@ -5,8 +5,10 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 import SiteCategories from './SiteCategories';
 import SearchButton from './SearchButton';
 import UserLogin from './UserLogin';
+import { useRouter } from 'next/navigation';
 
 const NavCategoriesBar = () => {
+  const router = useRouter();
   const dispatch = useDispatch();
 
   const expandBar =()=> {
@@ -16,13 +18,14 @@ const NavCategoriesBar = () => {
   return (
     <div className='flex inline'>
         <div className="inline w-full bg-[#FCF8F8] h-14 grid grid-flow-col auto-cols-max overflow-x-scroll no-scrollbar lg:gap-8 gap-4 lg:justify-start md:overflow-visible ">
-          <div className="hover:cursor-pointer  lg:ml-10 flex justify-start items-center gap-4" onClick={expandBar}>
+          <div className="hover:cursor-pointer ml-1 lg:ml-10 flex justify-start items-center gap-4" >
             <RxHamburgerMenu              
               className="h-6 w-6"
               color="black"
+              onClick={expandBar}
             />
-            <img src="/assets/images/logo.svg" className="hidden md:block h-14 w-32"></img>
-            <img src="/assets/images/logo-mb.svg" className="block md:hidden h-8 w-8"></img>
+            <img onClick={()=>{router.push("/")}} src="/assets/images/logo.svg" className="hidden md:block h-14 w-32"></img>
+            <img onClick={()=>{router.push("/")}} src="/assets/images/logo-mb.svg" className="block md:hidden h-8 w-8"></img>
           </div>
           <SiteCategories/>
           <SearchButton personalizedClass='hidden md:block md:!w-80'/>
