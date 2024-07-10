c5b87e49f3772c53ef00a4e6757d41fd52fcdeb9
Randall <rcotojimenez@gmail.com>
2024-07-10 17:35:38 -0600

    Cambios varios Randall

---
+++ main/components/HorizontalPromotionTags.jsx
@@ -0,0 +1,57 @@
+"use client"
+import React, { useEffect, useRef, useState } from 'react'
+import GenericAnimation from './GenericAnimation'
+
+const HorizontalPromotionTags = ({product}) => {
+    const parentHTML = useRef(null);
+    const [autoInterval, setAutoInterval] = useState(null);
+    const [tagsListToRender, setTagsListToRender] = useState([]);
+
+    useEffect(()=>{
+        const tempTags = [];
+        if(product.negotiable == "YES")
+            tempTags.push("Negociable, aproveche.");
+        
+        if(product.formatedEspecialPrice != 0)
+            tempTags.push("¡Rebajado!");    
+
+        setTagsListToRender(tempTags);
+
+    }, []);
+
+      const executeAutomaticScroll = () => {
+        try {
+          if (
+            Math.abs(
+              Math.ceil(parentHTML?.current?.scrollLeft) -
+                Math.ceil(
+                  parentHTML?.current?.scrollWidth - parentHTML?.current?.clientWidth
+                )
+            ) >= 1
+          ) {
+            parentHTML.current.scrollLeft += 1;
+          } else {
+            parentHTML.current.scrollLeft = 0;
+          }
+        } catch (error) {
+          clearInterval(autoInterval);
+        }
+       
+      };
+
+      useEffect(()=>{
+        const tempAutoInterval = setInterval(() => {   
+          executeAutomaticScroll();
+        }, 30);
+        setAutoInterval(tempAutoInterval);
+    }, [])
+  return (
+    <div ref={parentHTML} className='flex no-scrollbar overflow-x-auto h-6 w-full bg-transparent absolute bottom-0  left-0 w-full '>
+        {tagsListToRender.map((element, index)=>(
+             <GenericAnimation animation={""} message={element} key={index}/>
+        ))}
+    </div>
+  )
+}
+
+export default HorizontalPromotionTags
--- main/components/PromotionCard.jsx
+++ main/components/PromotionCard.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import GenericAnimation from "./GenericAnimation";
+import HorizontalPromotionTags from "./HorizontalPromotionTags";
 
 const PromotionCard = ({ product, index, callBackFunction }) => {
   const handleProductClick=()=>{
@@ -10,7 +11,9 @@ const PromotionCard = ({ product, index, callBackFunction }) => {
   return (
     <div key={`CARD-${index}`} className="shadow-gray-100 hover:border-gray-200 hover:shadow-xl promotion-image hover:cursor-pointer flex flex-col w-32 h-fit  lg:w-fit lg:h-fit mr-4 bg-slate-50 shadow-lg rounded-t-lg md:rounded-lg	 border border-1">
 
-      <div className="text-black h-8 flex items-start justify-center font-extrabold pl-2 pr-2 mb-2 mt-2">{product.productName}</div>
+      <div className="text-black h-8 flex items-start justify-center font-extrabold pl-2 pr-2 mb-2 mt-2">
+        {product.productName}
+        </div>
       <div className="h-10 lg:h-4 flex-row lg:flex inline gap-2 items-center justify-center mt-2 lg:mt-0 md:mb-2">        
         <div className="lg:inline text-center">
             <span className='text-black text-xs'>{product.currency}</span>
@@ -22,16 +25,15 @@ const PromotionCard = ({ product, index, callBackFunction }) => {
         </div>
         
       </div>
-      <div onClick={()=>{handleProductClick(product.productId)}} className="w-32 h-32 lg:w-64 lg:h-64 flex justify-center " >
-                <img
-                    alt={product.productDescription}
-                    src={product.productImage}
-                 className="w-32 h-32 lg:w-80 lg:h-64 aspect-square md:ml-4 md:mr-4"   
-                />
-                  {product.negotiable == "YES" && (
-                    <GenericAnimation message="Precio Negociable"></GenericAnimation>
-                  )}
-         </div>
+      <div onClick={()=>{handleProductClick(product.productId)}} className="relative w-32 h-32 lg:w-64 lg:h-64 flex justify-center " >
+            <HorizontalPromotionTags product={product}/>    
+        
+            <img
+              alt={product.productDescription}
+              src={product.productImage}
+            className="w-32 h-32 lg:w-80 lg:h-64 aspect-square md:ml-4 md:mr-4"   
+          />
+      </div>       
     </div>
   );
 };
