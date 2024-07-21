2f13717a38104c00b6840ffb3b07d78166b65043
Randall <rcotojimenez@gmail.com>
2024-07-20 21:44:30 -0600

    Test

--- main/app/productdetail/page.jsx
+++ main/app/productdetail/page.jsx
@@ -1,6 +1,4 @@
 import ProductDetailComponent from '@components/ProductDetailComponent';
-import axios from 'axios';
-import { useSearchParams } from 'next/navigation'
 
 export async function generateMetadata({ params, searchParams }) {
   // read route params
@@ -9,6 +7,9 @@ export async function generateMetadata({ params, searchParams }) {
   // fetch data
   const product = await fetch(`https://encuentralofacilcr.com/api/search/local/myitems/${storeId}/${productId}/`).then((res) => res.json())
   
+  const blob = b64toBlob(product.productImage[0].replace('data:image/jpeg;base64,', ''), 'data:image/jpeg;base64');
+  const blobUrl = URL.createObjectURL(blob);
+  
   // optionally access and extend (rather than replace) parent metadata
   //const previousImages = (await parent).openGraph?.images || []
  
@@ -16,7 +17,7 @@ export async function generateMetadata({ params, searchParams }) {
     title: "Encuéntralo Fácil CR - " + product.productName,
     description:product.productDescription,
     openGraph: {
-      images: product.productImage,
+      images: blobUrl,
     },
   }
 }
@@ -28,3 +29,24 @@ const Details = () => {
 };
 
 export default Details;
+
+
+const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
+  const byteCharacters = atob(b64Data);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+    
+  const blob = new Blob(byteArrays, {type: contentType});
+  return blob;
+}
