9d874918ee7f59170519be77df54654df0592ad4
mmasis <>
2024-07-23 21:51:34 -0600

    Fix issue with prices

--- main/components/ProductCard.jsx
+++ main/components/ProductCard.jsx
@@ -6,7 +6,6 @@ import { useState } from "react";
 
 
 const ProductCard = ({ logo, product, index, adminMode, callBackFunction }) => {
-
   const [showShareModal, setShowShareModal] = useState(false);
   const handleProductClick=(url)=>{
     if(adminMode || product.isLocal){
@@ -25,9 +24,9 @@ const ProductCard = ({ logo, product, index, adminMode, callBackFunction }) => {
       className={` hover:cursor-pointer product-image flex items-center justify-center block bg-white rounded-lg p-2 shadow shadow-gray-100 border border-gray-200 hover:border-gray-200 `}
     >
      <div key={`CARD-${index}`} className="flex flex-col w-32 h-64 lg:w-48 lg:h-80 gap-2">
-         <div onClick={()=>{handleProductClick(product.productId)}} className="w-32 h-32 lg:w-48 lg:h-48" >
+         <div onClick={()=>{handleProductClick(product.productId)}} className="w-32 h-32 lg:w-48 lg:h-48 lg:max-w-48 lg:max-h-48" >
           <div className="p-2 absolute left-0 top-0 w-full h-full flex items-center justify-center container-blur product-image">
-            <img
+            <img className="lg:max-w-48 lg:max-h-48"
               alt="product.productDescription"
               loading="lazy"
               src={product.productImage}
@@ -40,10 +39,10 @@ const ProductCard = ({ logo, product, index, adminMode, callBackFunction }) => {
         {product.isLocal && <BsShare className="absolute top-4 right-4 w-4 h-4" color="black" onClick={()=>{setShowShareModal(true)}}/>      }
       </div>
       <div className="rh-10 lg:h-4 flex-row lg:flex inline gap-2 items-center justify-center mt-2 lg:mt-0">        
-            {product.formatedEspecialPrice && 
-             product.formatedEspecialPrice != 0 && (
+            {Boolean(product.formatedEspecialPrice && 
+             product.formatedEspecialPrice != 0) && (
               <div className="lg:inline text-center text-black">
-                <span className='text-xs'>{product.currency == "CRC" ? "₡" : "$"}</span>
+                <span className='text-xs'>{product.currency == "CRC" ? "₡" : product.currency}</span>
                 <span className="line-through text-xs">
                     {product.formatedPrice}
                 </span>
@@ -51,10 +50,10 @@ const ProductCard = ({ logo, product, index, adminMode, callBackFunction }) => {
             )}
         
         <div className="lg:inline text-center text-black">
-            <span className='text-xs'>{product.currency == "CRC" ? "₡" : "$"}</span>
+            <span className='text-xs'>{product.currency == "CRC" ? "₡" : product.currency}</span>
             <span className='text-xs'> 
-            {product.formatedEspecialPrice &&
-             product.formatedEspecialPrice != 0
+            {Boolean(product.formatedEspecialPrice && 
+             product.formatedEspecialPrice != 0)
                 ? product.formatedEspecialPrice
                 : product.formatedPrice} 
             </span>
